f85ae1352347f5442d6173d7abe9d73ab3728e4f
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-08-08 11:30:37 +0200

    Update readme.md and styles.css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zad-15.9
+https://krystynaszur.github.io/zad-15.9/
--- script.js
+++ script.js
@@ -56,7 +56,7 @@ class App extends React.Component {
   class User extends React.Component {
     render() {
       return (
-        <div>
+        <div className="user">
           <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
           <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
         </div>
